54ab4fa3c5892640bd39ca4ca0ffae44895e8f53
VoyageDev <louizkosasih@gmail.com>
2026-02-20 14:22:56 +0700

    update meta tag and logo favicon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel';
 
-// https://astro.build/config
 export default defineConfig({
     site: 'https://borneox.biz.id',
     base: '/',
+    
+    adapter: vercel()
 });
