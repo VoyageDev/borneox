00d49ae41de9f20d395b8ee8d6494e006eb16385
VoyageDev <louizkosasih@gmail.com>
2026-02-20 16:03:25 +0700

    adding sitemap.xml

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,9 +2,11 @@
 import { defineConfig } from 'astro/config';
 import vercel from '@astrojs/vercel';
 
+import sitemap from '@astrojs/sitemap';
+
 export default defineConfig({
-    site: 'https://borneox.biz.id',
-    base: '/',
-    
-    adapter: vercel()
+  site: 'https://borneox.biz.id',
+  base: '/',
+  adapter: vercel(),
+  integrations: [sitemap()]
 });
